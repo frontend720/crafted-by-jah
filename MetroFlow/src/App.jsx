99fb010aa82b2666371f81688dc95be9c34f315b
frontend720 <frontend720@gmail.com>
2025-06-30 11:20:15 -0600

    updating UI, adding JSON data

--- MetroFlow/src/App.jsx
+++ MetroFlow/src/App.jsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "swiper/css";
+import "swiper/css/thumbs";
+import data from "./details.json";
+import { BiDirections, BiSolidDirections } from "react-icons/bi";
+import { BsBuildingsFill, BsBuildings } from "react-icons/bs";
+import { RiShoppingBagLine, RiShoppingBag2Line } from "react-icons/ri";
+import { HiOutlineLibrary, HiLibrary } from "react-icons/hi";
+import { IoMoon } from "react-icons/io5"
+import { IoSunny } from "react-icons/io5";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [slide, setSlide] = useState(0);
+  const [line, setLine] = useState(0);
+  const [line_detail, set_line_detail] = useState(0)
+
+  function lineOne() {
+    setLine(0);
+    set_line_detail(0)
+  }
+
+  function lineTwo() {
+    setLine(1);
+    set_line_detail(1)
+    
+  }
+
+  function lineThree() {
+    setLine(2);
+    set_line_detail(2)
+  }
+
+  function lineFour() {
+    setLine(3);
+    set_line_detail(3)
+  }
+
+  function slideOne() {
+    setSlide(0);
+  }
+
+  function slideTwo() {
+    setSlide(1);
+  }
+
+  function slideThree() {
+    setSlide(2);
+  }
+
+  function slideFour() {
+    setSlide(3);
+  }
+
+  const currentLineInfo = data.metro_schedule.light_rail_lines[line]?.info;
+
+
+  const getIntroText = () => {
+    return (
+      currentLineInfo?.[slide]?.intro ||
+      "No information available for this slide."
+    );
+  };
+
+  const [theme, setTheme] = useState(true)
+  function themeTrigger(){
+    setTheme(prev => !prev)
+  }
+
+console.log(data.metro_schedule.light_rail_lines[1].theme)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={
+      // {backgroundImage: data.metro_schedule.light_rail_lines[theme === false ? 0 : 1].theme.dark}
+      {
+        backgroundImage: data.metro_schedule.line_cards[line],
+        // background: data.metro_schedule.line_colors[line]
+  
+      }
+     } className="container">
+      <div onClick={themeTrigger} className="toggle-container">
+        {theme ? <IoMoon color="#EFEBCE" /> : <IoSunny color="#F8E16C" />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="card-container">
+        <img style={{position: "absolute", zIndex: -1}} src={data.metro_schedule.line_cards[line]} width={"100%"} height={"100%"} alt="" />
+        <div className="summary-container">
+          <div className="line-container">
+            <div onClick={lineOne} className="line">
+              Red
+            </div>
+            <div onClick={lineTwo} className="line">
+              Blue
+            </div>
+            <div onClick={lineThree} className="line">
+              Green
+            </div>
+            <div onClick={lineFour} className="line">
+              Yellow
+            </div>
+          </div>
+          <div
+            className="line"
+            style={{
+              borderTop: `8px solid ${data.metro_schedule.line_colors[line]}`,
+              marginTop: 20,
+            }}
+          ></div>
+          <h1 className="line-name">
+            {data.metro_schedule.light_rail_lines[line].name}
+          </h1>
+          <div className="text-container">
+            <label className="intro-text">
+              
+              {getIntroText()}
+            </label>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="guide-buttons" onClick={slideOne}>
+            <BiDirections
+              color={data.metro_schedule.light_rail_lines[0].info[0].color}
+            />
+          </button>
+          <button className="guide-buttons" onClick={slideTwo}>
+            <BsBuildings
+              color={data.metro_schedule.light_rail_lines[0].info[1].color}
+            />
+          </button>
+          <button className="guide-buttons" onClick={slideThree}>
+            <RiShoppingBagLine
+              color={data.metro_schedule.light_rail_lines[0].info[2].color}
+            />
+          </button>
+          <button className="guide-buttons" onClick={slideFour}>
+            <HiLibrary
+              color={data.metro_schedule.light_rail_lines[0].info[3].color}
+            />
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
